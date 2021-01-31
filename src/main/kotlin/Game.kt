6413196ce3856ab803b8b4d7cf3952ff85ba166e
iteo solution <iteosolution@gmail.com>
2021-01-31 22:46:14 +0900

    Initial commit

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    val name = "마드리길"
+    var healthPoints = 100
+
+    if (healthPoints == 100) {
+        println(name + " 최상의 상태임!")
+
+    } else {
+        println(name + " 최악의 상태임!")
+    }
+
+}
